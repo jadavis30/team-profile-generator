9ced73fdd8bf57e53d417547f9decc0258a6610e
Jared Davis <jared.davis21@yahoo.com>
2021-01-11 22:54:36 -0500

    inquirer started

--- index.js
+++ index.js
@@ -1,3 +1,98 @@
-const Generator = require("./lib/Generator");
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
 
-new Generator().promptUser();
+const questions = [
+    {    
+        type: "input",
+        name: "team",
+        message:"What is the name of your team or company?",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide a name for your team or company.");
+            }
+            return true;
+            },
+        },
+        {    
+        type: "input",
+        name: "manager",
+        message:"What is the name of the team's manager?",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide their name.");
+            }
+            return true;
+            },
+        },
+        {    
+        type: "input",
+        name: "id",
+        message:"What is the manager's employee ID?",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide a valid number.");
+            }
+            return true;
+            },
+        },
+        {    
+        type: "input",
+        name: "email",
+        message:"What is the manager's e-mail address?",
+        validate: function (answer) {
+            if (answer.length < 1)  {
+                return console.log("Please provide a valid e-mail.");
+            }
+            return true;
+            },
+        },
+        {    
+        type: "input",
+        name: "office",
+        message:"What is the manager's office number?",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please provide an office number.");
+            }
+            return true;
+            },
+        },
+        {    
+        type: "list",
+        name: "build",
+        message:"Build your team by selecting 'Engineer' or 'Intern' to add members; select 'Finalize' to generate the page!",
+        choices: ["Engineer", "Intern", "Finalize"],
+        },  
+     
+]
+
+//Write HTML file
+function writeToFile(fileName, data) {   
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        
+        console.log("HTML complete! Check Team.html to see its output.");
+    });
+};    
+        
+const writeFileAsync = util.promisify(writeToFile);
+
+// Initialize app
+async function init() {
+    try {
+        //Inquirer ?s
+        const userResponses = await inquirer.prompt(questions);
+        //Pass Inquirer ?s to generateMarkdown
+        const html = generateHTML(userResponses);
+        //Write markdown to file
+        await writeFileAsync("index.html", html);
+    } catch (error) {
+        console.log(error);
+    }    
+}; 
+
+// Function call to initialize app
+init();
